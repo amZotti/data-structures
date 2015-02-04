becae7b33a7733661e6423f444ee535c018dbb56
amZotti <anthonymzotti@gmail.com>
2015-02-04 15:07:01 -0800

    Stack data structure using functional-shared pattern passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var currentInstance = {
+    index : 0,
+    storage : {},
+    push : stackMethods.push,
+    pop : stackMethods.pop,
+    size : stackMethods.size
+  };
+  return currentInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value){
+    this.storage[this.index++] = value;
+  },
+
+  pop : function(){
+    if (this.index !== 0)
+      return this.storage[--this.index];
+  },
+
+  size : function(){
+    return this.index;
+  }
+};
 
 
