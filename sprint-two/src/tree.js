f1fdf98c915b52255d797102f31947276a72dfb7
amZotti <anthonymzotti@gmail.com>
2015-02-04 19:53:25 -0800

    Tree data structure passes all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,9 +2,9 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,11 +15,18 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var childNode = Tree(value);
+  this.children.push(childNode);
 };
 
 treeMethods.contains = function(target){
-
+  var results = (this.value === target);
+  if (!results) {
+    for (var i = 0; i < this.children.length; i += 1) {
+      results = results || this.children[i].contains(target);
+    }
+  }
+  return results;
 };
 
 
