473e4db0f59035697c6f32d48b1ba12da6f6e358
amZotti <anthonymzotti@gmail.com>
2015-02-04 16:41:13 -0800

    LinkedList data structure using passes all tests
    linked list done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node();
+    node.value = value;
+    if (!(list.tail === null)) {
+      list.tail.next = node;
+    }
+    if (list.head === null){
+      list.head = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var node = list.head;
+
+    if (!(list.head === null)) {
+      list.head = list.head.next;
+      return node.value;
+    } else {
+      list.tail = null;
+    }
+    return null;
   };
 
   list.contains = function(target){
+    var trav = list.head;
+
+    while ((trav !== null) && (trav.value !== target)) {
+      trav = trav.next;
+    }
+
+    if (trav === null) {
+      return false;
+    }
+
+    return true;
   };
 
   return list;
