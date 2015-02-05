e04f6bbc380ad19ebac45d05d4bcdb97f84082bd
amZotti <anthonymzotti@gmail.com>
2015-02-04 16:06:57 -0800

    Stack data structure using psuedo-classical pattern passes all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,17 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
 
+Stack.prototype.pop = function(){
+  if (this.index !== 0)
+    return this.storage[--this.index];
+};
 
+Stack.prototype.push = function(value){
+  this.storage[this.index++] = value;
+};
+
+Stack.prototype.size = function(){
+  return this.index;
+}
