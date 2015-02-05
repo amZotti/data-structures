2015d0cf6fd3f645cf8d31debf6e996230d6d88c
amZotti <anthonymzotti@gmail.com>
2015-02-05 11:01:24 -0800

    Set datastructure passes all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+  return this._storage.indexOf(item) !== -1;
 };
 
 setPrototype.remove = function(item){
+  var index = this._storage.indexOf(item);
+  console.log(index);
+  if (index !== -1){
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
