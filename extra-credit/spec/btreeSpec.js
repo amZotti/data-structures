22a992224b87ac290386f1b834df6a48ff0e5afc
amZotti <anthonymzotti@gmail.com>
2015-02-07 11:54:08 -0800

    Create spec for BTree data structore

--- extra-credit/spec/btreeSpec.js
+++ extra-credit/spec/btreeSpec.js
@@ -2,11 +2,23 @@ describe('b-tree', function(){
   var btree
 
   beforeEach(function(){
-    btree = new BTree();
+    btree = new BTree(3);
   });
 
-  it('Should have a root node.', function() {
-    expect(!!btree.root).to.not.equal(false);
+  it('Should construct a btree correctly when values are inserted', function() {
+    btree.add(5);
+    btree.add(1);
+    btree.add(111);
+    btree.add(444);
+    btree.add(44343);
+    btree.add(2);
+    btree.add(3);
+    btree.add(6);
+    btree.add(8);
+    btree.add(15);
+    btree.add(10);
+
+    console.dir(btree.root);
   });
 
 });
