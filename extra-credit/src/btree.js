d8b07ca94d3c8cfb05c8a12ad3db988959929e66
amZotti <anthonymzotti@gmail.com>
2015-02-07 11:40:22 -0800

    Begin refactoring BTree datastructure insertion

--- extra-credit/src/btree.js
+++ extra-credit/src/btree.js
@@ -21,47 +21,70 @@ function BTree(maxSize) {
 
   BTreeNode.prototype.doesChildHaveRoom = function(insertionIndex) {
     return this.children[insertionIndex].values.length < this.children[insertionIndex].maxSize;
-  } ;
+  };
 
-  BTreeNode.prototype.add = function(value) {
+  BTreeNode.prototype.attemptChildInsertion = function(value) {
     if (this.children.length !== 0) {
       var insertionIndex = this.findInsertionIndex(value);
       if (this.doesChildHaveRoom(insertionIndex)) {
         this.children[insertionIndex].add(value);
-        return;
+        return true;
       }
     }
-    this.sortedInsert(this.values, value);
-    if (this.values.length >= this.maxSize) {
-      var newSibling = new BTreeNode(this.maxSize);
-      var middleElementIndex = Math.floor(this.values.length / 2);
-      for (var i = 0; i < middleElementIndex; i += 1) {
-        newSibling.add(this.values[i]);
-      }
-      if (this.parent === null) {
-        this.parent = new BTreeNode(this.maxSize);
-        this.parent.add(this.values[middleElementIndex]);
-        this.parent.children.push(newSibling);
-        this.parent.children.push(this);
-        tree.root = this.parent;
-      } else {
-        var siblingIndex = this.findChildIndex(this) - 1;
-        this.parent.add(this.values[middleElementIndex]);
-        this.parent.children.splice(siblingIndex, 0, newSibling);
-      }
-      newSibling.parent = this.parent;
-      this.values = this.values.slice(middleElementIndex + 1);
+    return false;
+  };
 
-      if (this.children.length === 0) {
-        return;
+  BTreeNode.prototype.populateSibling = function(sibling, index) {
+    for (var i = 0; i < index; i += 1) {
+      sibling.add(this.values[i]);
+    }
+  };
+
+  BTreeNode.prototype.createParent = function(sibling, index) {
+    this.parent = new BTreeNode(this.maxSize);
+    this.parent.add(this.values[index]);
+    this.parent.children.push(sibling);
+    this.parent.children.push(this);
+    tree.root = this.parent;
+  };
+
+  BTreeNode.prototype.splitNode = function() {
+    var newSibling = new BTreeNode(this.maxSize);
+    var middleElementIndex = Math.floor(this.values.length / 2);
+
+    this.populateSibling(newSibling, middleElementIndex);
+
+    if (this.parent === null) {
+      this.createParent(newSibling, middleElementIndex);
+    } else {
+      var siblingIndex = this.findChildIndex(this) - 1;
+      this.parent.add(this.values[middleElementIndex]);
+      this.parent.children.splice(siblingIndex, 0, newSibling);
+    }
+    newSibling.parent = this.parent;
+    this.values = this.values.slice(middleElementIndex + 1);
+
+    if (this.children.length === 0) {
+      return;
+    }
+    else {
+      for (var k = 0;k <= middleElementIndex;k++) {
+        newSibling.children.push(this.children[k]);
+        this.children[k].parent = newSibling;
       }
-      else {
-        for (var k = 0;k <= middleElementIndex;k++) {
-          newSibling.children.push(this.children[k]);
-          this.children[k].parent = newSibling;
-        }
-        this.children = this.children.slice(middleElementIndex + 1);
-      }
+      this.children = this.children.slice(middleElementIndex + 1);
+    }
+  };
+
+  BTreeNode.prototype.add = function(value) {
+    if (this.attemptChildInsertion(value)) {
+      return;
+    }
+
+    this.sortedInsert(this.values, value);
+
+    if (this.values.length >= this.maxSize) {
+      this.splitNode();
     }
   };
 
